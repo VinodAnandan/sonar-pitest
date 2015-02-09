32d94c37e732a7695a224cd5d664add7b218cbd7
Gerald Muecke <gerald@moskito.li>
2015-02-09 18:14:15 +0100

    added unit tests

--- src/test/java/org/sonar/plugins/pitest/model/MutantBuilderTest.java
+++ src/test/java/org/sonar/plugins/pitest/model/MutantBuilderTest.java
@@ -1,7 +1,27 @@
+/*
+ * Sonar Pitest Plugin
+ * Copyright (C) 2015 SonarCommunity
+ * dev@sonar.codehaus.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
 package org.sonar.plugins.pitest.model;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
@@ -39,69 +59,99 @@ public class MutantBuilderTest {
     }
 
     @Test
-    public void testMutantStatusString() throws Exception {
+    public void testMutantStatusString_validStatus() throws Exception {
 
-        throw new RuntimeException("not yet implemented");
+        subject.mutantStatus("KILLED");
+        assertEquals(MutantStatus.KILLED, subject.build().getMutantStatus());
+    }
+
+    @Test
+    public void testMutantStatusString_invalidStatus_unknown() throws Exception {
+
+        subject.mutantStatus("invalidStatus");
+        assertEquals(MutantStatus.UNKNOWN, subject.build().getMutantStatus());
     }
 
     @Test
     public void testInSourceFile() throws Exception {
 
-        throw new RuntimeException("not yet implemented");
+        subject.inSourceFile("someSource.java");
+        assertEquals("someSource.java", subject.build().getSourceFile());
     }
 
     @Test
     public void testInClass() throws Exception {
 
-        throw new RuntimeException("not yet implemented");
+        subject.inClass("some.package.SomeClass");
+        assertEquals("some.package.SomeClass", subject.build().getMutatedClass());
     }
 
     @Test
     public void testInMethod() throws Exception {
 
-        throw new RuntimeException("not yet implemented");
+        subject.inMethod("aMethod");
+        assertEquals("aMethod", subject.build().getMutatedMethod());
     }
 
     @Test
     public void testWithMethodParameters() throws Exception {
 
-        throw new RuntimeException("not yet implemented");
+        subject.withMethodParameters("methodDescription");
+        assertEquals("methodDescription", subject.build().getMethodDescription());
     }
 
     @Test
     public void testInLine() throws Exception {
 
-        throw new RuntimeException("not yet implemented");
+        subject.inLine(123);
+        assertEquals(123, subject.build().getLineNumber());
     }
 
     @Test
     public void testUsingMutatorMutator() throws Exception {
 
-        throw new RuntimeException("not yet implemented");
+        // prepare
+        final Mutator mutator = Mutator.find("ARGUMENT_PROPAGATION");
+        assertNotNull(mutator);
+        // act
+        subject.usingMutator(Mutator.find("ARGUMENT_PROPAGATION"));
+        // assert
+        assertEquals(mutator, subject.build().getMutator());
     }
 
     @Test
     public void testUsingMutatorString() throws Exception {
 
-        throw new RuntimeException("not yet implemented");
+        final Mutator mutator = Mutator.find("org.pitest.mutationtest.engine.gregor.mutators.RemoveConditionalMutator");
+        assertNotNull(mutator);
+
+        subject.usingMutator("org.pitest.mutationtest.engine.gregor.mutators.RemoveConditionalMutator");
+        assertEquals(mutator, subject.build().getMutator());
+    }
+
+    @Test
+    public void testUsingMutatorString_withSuffix() throws Exception {
+
+        final Mutator mutator = Mutator.find("org.pitest.mutationtest.engine.gregor.mutators.RemoveConditionalMutator");
+        assertNotNull(mutator);
+
+        subject.usingMutator("org.pitest.mutationtest.engine.gregor.mutators.RemoveConditionalMutator_A_SUFFIX");
+        assertEquals(mutator, subject.build().getMutator());
+        assertEquals("A_SUFFIX", subject.build().getMutatorSuffix());
     }
 
     @Test
     public void testAtIndex() throws Exception {
 
-        throw new RuntimeException("not yet implemented");
+        subject.atIndex(123);
+        assertEquals(123, subject.build().getIndex());
     }
 
     @Test
     public void testKilledBy() throws Exception {
 
-        throw new RuntimeException("not yet implemented");
-    }
-
-    @Test
-    public void testBuild() throws Exception {
-
-        throw new RuntimeException("not yet implemented");
+        subject.killedBy("killingTest");
+        assertEquals("killingTest", subject.build().getKillingTest());
     }
 
 }
--- src/test/java/org/sonar/plugins/pitest/model/MutatorTest.java
+++ src/test/java/org/sonar/plugins/pitest/model/MutatorTest.java
@@ -20,7 +20,14 @@
 package org.sonar.plugins.pitest.model;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Collection;
 
 import org.junit.Test;
 
@@ -59,6 +66,98 @@ public class MutatorTest {
         assertEquals("org.pitest.mutationtest.engine.gregor.mutators.ArgumentPropagationMutator",
                 mutator.getClassName());
         assertNotNull(mutator.getViolationDescription());
+    }
+
+    @Test
+    public void testGetAllMutators() throws Exception {
+
+        // act
+        final Collection<Mutator> mutators = Mutator.getAllMutators();
+
+        // assert
+        assertNotNull(mutators);
+        assertFalse(mutators.isEmpty());
+
+    }
+
+    @Test
+    public void testGetId() throws Exception {
+
+        final Mutator mutator = Mutator.find("ARGUMENT_PROPAGATION");
+        assertEquals("ARGUMENT_PROPAGATION", mutator.getId());
+    }
+
+    @Test
+    public void testGetMutatorDescriptionLocation() throws Exception {
+
+        final Mutator mutator = Mutator.find("ARGUMENT_PROPAGATION");
+        final URL descriptorLocation = mutator.getMutatorDescriptionLocation();
+        assertNotNull(descriptorLocation);
+    }
+
+    @Test
+    public void testGetMutatorDescriptionAsStream() throws Exception {
+
+        final Mutator mutator = Mutator.find("ARGUMENT_PROPAGATION");
+        final InputStream descStream = mutator.getMutatorDescriptionAsStream();
+        assertNotNull(descStream);
+    }
+
+    @Test
+    public void testGetMutatorDescription() throws Exception {
+
+        final Mutator mutator = Mutator.find("ARGUMENT_PROPAGATION");
+        final String desc = mutator.getMutatorDescription();
+        assertNotNull(desc);
+    }
+
+    @Test
+    public void testGetViolationDescription() throws Exception {
+
+        final Mutator mutator = Mutator.find("ARGUMENT_PROPAGATION");
+        final String violationDesc = mutator.getViolationDescription();
+        assertNotNull(violationDesc);
+    }
+
+    @Test
+    public void testGetName() throws Exception {
+
+        final Mutator mutator = Mutator.find("ARGUMENT_PROPAGATION");
+        final String name = mutator.getName();
+        assertEquals("Argument Propagation Mutator", name);
+    }
+
+    @Test
+    public void testGetClassName() throws Exception {
+
+        final Mutator mutator = Mutator.find("ARGUMENT_PROPAGATION");
+        final String className = mutator.getClassName();
+        assertEquals("org.pitest.mutationtest.engine.gregor.mutators.ArgumentPropagationMutator", className);
+    }
+
+    @Test
+    public void testHashCode() throws Exception {
+
+        final Mutator argumentPropagation = Mutator.find("ARGUMENT_PROPAGATION");
+        final Mutator conditionalsBoundary = Mutator.find("CONDITIONALS_BOUNDARY");
+
+        assertEquals(argumentPropagation.hashCode(), argumentPropagation.hashCode());
+        assertNotEquals(argumentPropagation.hashCode(), conditionalsBoundary.hashCode());
+    }
+
+    @Test
+    public void testEquals_different_false() throws Exception {
+
+        final Mutator argumentPropagation = Mutator.find("ARGUMENT_PROPAGATION");
+        final Mutator conditionalsBoundary = Mutator.find("CONDITIONALS_BOUNDARY");
+        assertFalse(argumentPropagation.equals(conditionalsBoundary));
+    }
+
+    @Test
+    public void testEquals_same_true() throws Exception {
+
+        final Mutator argumentPropagation = Mutator.find("ARGUMENT_PROPAGATION");
+        assertTrue(argumentPropagation.equals(argumentPropagation));
     }
 
 }
