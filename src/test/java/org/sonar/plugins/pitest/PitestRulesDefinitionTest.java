51ba62aa54f459c0624c0095152cae5404849bb5
Gerald Muecke <gerald@moskito.li>
2015-02-18 07:47:54 +0100

    SONARPLUGINS-2869 added rules def unit test

--- src/test/java/org/sonar/plugins/pitest/PitestRulesDefinitionTest.java
+++ src/test/java/org/sonar/plugins/pitest/PitestRulesDefinitionTest.java
@@ -19,13 +19,39 @@
  */
 package org.sonar.plugins.pitest;
 
-import org.junit.Ignore;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.ArgumentCaptor.forClass;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyBoolean;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Answers;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
+import org.mockito.Matchers;
 import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
 import org.sonar.api.config.Settings;
+import org.sonar.api.rule.RuleStatus;
+import org.sonar.api.server.debt.DebtRemediationFunction;
+import org.sonar.api.server.rule.RulesDefinition.Context;
+import org.sonar.api.server.rule.RulesDefinition.NewRepository;
+import org.sonar.api.server.rule.RulesDefinition.NewRule;
 import org.sonar.api.server.rule.RulesDefinitionXmlLoader;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -34,16 +60,103 @@ public class PitestRulesDefinitionTest {
     @Mock
     private Settings settings;
 
+    @Mock(answer = Answers.RETURNS_DEEP_STUBS)
+    private NewRepository newRepository;
+
+    @Mock
+    private Context context;
+
+    @Mock(answer = Answers.RETURNS_DEEP_STUBS)
+    private NewRule rule;
+
     @Mock
     private RulesDefinitionXmlLoader xmlLoader;
+
     @InjectMocks
-    private PitestRulesDefinition pitestRulesDefinition;
+    private PitestRulesDefinition subject;
+
+    @Before
+    public void setUp() throws Exception {
+
+        setupRule(rule);
+
+    }
+
+    /**
+     * Sets up the rule by returning the rule itself on every setter to allow proper builder pattern usage
+     *
+     * @param rule
+     */
+    private void setupRule(final NewRule rule) {
+
+        doReturn(rule).when(rule).setDebtRemediationFunction(any(DebtRemediationFunction.class));
+        doReturn(rule).when(rule).setDebtSubCharacteristic(anyString());
+        doReturn(rule).when(rule).setEffortToFixDescription(anyString());
+        doReturn(rule).when(rule).setHtmlDescription(anyString());
+        doReturn(rule).when(rule).setHtmlDescription(any(URL.class));
+        doReturn(rule).when(rule).setInternalKey(anyString());
+        doReturn(rule).when(rule).setMarkdownDescription(anyString());
+        doReturn(rule).when(rule).setMarkdownDescription(any(URL.class));
+        doReturn(rule).when(rule).setName(anyString());
+        doReturn(rule).when(rule).setSeverity(anyString());
+        doReturn(rule).when(rule).setStatus(any(RuleStatus.class));
+        doReturn(rule).when(rule).setTags(Matchers.<String[]> anyVararg());
+        doReturn(rule).when(rule).setTemplate(anyBoolean());
+    }
 
     @Test
-    @Ignore
     public void testDefine() throws Exception {
 
-        throw new RuntimeException("not yet implemented");
+        // prepare
+        when(context.createRepository("pitest", "java")).thenReturn(newRepository);
+        when(newRepository.setName(anyString())).thenReturn(newRepository);
+        final List<NewRule> rules = new ArrayList<>();
+        when(newRepository.createRule(anyString())).then(new Answer<NewRule>() {
+
+            @Override
+            public NewRule answer(final InvocationOnMock invocation) throws Throwable {
+
+                rules.add(rule);
+                return rule;
+            }
+
+        });
+        when(newRepository.rules()).thenReturn(rules);
+
+        // act
+        subject.define(context);
+
+        // assert
+        verify(newRepository).setName("Pitest");
+        verifyXmlLoader();
+        verifyRules(rules);
+        verify(newRepository).done();
+    }
+
+    private void verifyRules(final List<NewRule> rules) {
+
+        // 17 Mutator Rules, see mutator-def.xml
+        verify(newRepository, times(17)).createRule(anyString());
+        assertEquals(17, rules.size());
+
+        // as we reuse the same rule, we count the total method invocation for each "rule"
+        verify(rule, times(17)).setDebtRemediationFunction(any(DebtRemediationFunction.class));
+        verify(rule, times(17)).setEffortToFixDescription(anyString());
+        verify(rule, times(17)).setDebtSubCharacteristic("UNIT_TESTS");
+        // 5 experimental mutators = beta rules
+        verify(rule, times(5)).setStatus(RuleStatus.BETA);
+
+    }
+
+    private void verifyXmlLoader() {
+
+        final ArgumentCaptor<NewRepository> captorForRepo = forClass(NewRepository.class);
+        final ArgumentCaptor<InputStream> captorForStream = forClass(InputStream.class);
+        final ArgumentCaptor<String> captorForEncoding = forClass(String.class);
+        verify(xmlLoader).load(captorForRepo.capture(), captorForStream.capture(), captorForEncoding.capture());
+        assertEquals(newRepository, captorForRepo.getValue());
+        assertEquals("UTF-8", captorForEncoding.getValue());
+        assertNotNull(captorForStream.getValue());
     }
 
 }
